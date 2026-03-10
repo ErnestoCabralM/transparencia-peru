9214f6960f3ea2af46540cbf97b95be0d573559e
Ernesto Cabral <ernestocabralm@gmail.com>
2026-03-10 01:19:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// script
+fetch("data/pedidos.json")
+  .then(response => response.json())
+  .then(data => {
+
+    let contenedor = document.getElementById("lista");
+
+    data.forEach(pedido => {
+
+      let div = document.createElement("div");
+
+      div.innerHTML = `
+        <h3>${pedido.resumen}</h3>
+        <p><b>Entidad:</b> ${pedido.entidad}</p>
+        <p><b>Resultado:</b> ${pedido.resultado}</p>
+        <p><b>Estado:</b> ${pedido.estado_actual}</p>
+        <hr>
+      `;
+
+      contenedor.appendChild(div);
+
+    });
+
+});
